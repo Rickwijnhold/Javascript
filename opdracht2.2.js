badcf470b58caeef64150d1c6818fc3b9d9729ac
RIckwijnhold <rick.wijnhold@student.rocva.nl>
2019-06-17 08:52:41 +0200

    Opdracht 2.2 javascript

---
+++ opdracht2.2.js
@@ -0,0 +1,4 @@
+document.getElementById("blok1").style.backgroundColor = "blue";
+document.getElementById("blok2").style.backgroundColor = "green";
+document.getElementById("blok3").style.backgroundColor = "yellow";
+document.getElementById("blok4").style.backgroundColor = "red";
